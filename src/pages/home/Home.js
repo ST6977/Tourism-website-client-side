336f472112d99b39876519d289f10469af0e34ab
ST6977 <sumaiyat610@gmail.com>
2021-11-27 23:46:59 -0800

    vacation added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,36 +3,33 @@ import axios from "axios";
 import Events from "./event/Events";
 import Footer from "../../Footer/Footer";
 
-
 const Home = () => {
   const [events, setEvents] = useState([]);
   useEffect(() => {
     axios
       .get("https://creepy-cat-14143.herokuapp.com/events")
       .then((res) => setEvents(res.data))
-      .catch(error =>console.log(error))
+      .catch((error) => console.log(error));
   }, []);
 
-
-  console.log(events) ;
+  console.log(events);
   return (
- 
-///banner
+    ///banner
     <div className=" ">
       {/* Banner  */}
       <section>
-
         <div>
-          <img className="w-100 h-30" src="https://holidaymatrix.co.in/img/slide-image-3.jpg" alt="" />
+          <img
+            className="w-100 h-30"
+            src="https://holidaymatrix.co.in/img/slide-image-3.jpg"
+            alt=""
+          />
         </div>
       </section>
 
-
-
-
-
       <h1 className="text-4xl text-center my-10 font-bold">
-      Enjoy Your Vacation Tour With Us!And Make Your Trip Memorable With ShareTrip!!
+        Enjoy Your Vacation Tour With Us!And Make Your Trip Memorable With
+        ShareTrip!!Happy Vacation!!
       </h1>
 
       <section className="container mb-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mx-auto">
@@ -47,8 +44,6 @@ const Home = () => {
           />
         ))}
       </section>
-
-
     </div>
   );
 };
